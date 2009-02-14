f209c49f8988a9c64fcad3f48accca095fb1fbd1
Michael Barton <mail@michaelbarton.me.uk>
2009-02-14 19:48:01 +0000

    Updated phylogeny plot

--- amino_acid_usage/plot/phylogeny.R
+++ amino_acid_usage/plot/phylogeny.R
@@ -6,6 +6,11 @@ data <- read.csv('data/amino_acid_usage.csv')
 melted <- melt(data,id.var=c('species','acid','weight'))
 usage <- cast(melted,species ~ acid + variable)
 
+species <- usage[,1]
+usage <-  as.matrix(usage[,2:21])
+
+rownames(usage) <- species
+colnames(usage) <- levels(data$acid)
 
 postscript("results/phylogeny.eps",width=5,height=5,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
 

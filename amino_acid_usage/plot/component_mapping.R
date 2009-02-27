7909e4ed2c0cd540f52121343e033ae47a76abf8
Michael Barton <mail@michaelbarton.me.uk>
2009-02-27 09:57:07 +0000

    Plot just amino acid projections

--- amino_acid_usage/plot/component_mapping.R
+++ amino_acid_usage/plot/component_mapping.R
@@ -3,7 +3,7 @@ library(reshape)
 
 data <- read.csv('data/amino_acid_usage.csv')
 
-melted <- melt(data,id.var=c('species','acid','weight'))
+melted <- melt(data,id.var=c('species','acid','weight','glucose','ammonium','sulphur'))
 usage <- cast(
   melted,
   species ~ acid + variable,
@@ -16,25 +16,6 @@ colnames(usage) <- levels(data$acid)
 
 decomp <- svd(usage)
 
-postscript("results/species_difference.eps",width=5,height=5,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
-
-plot(
-  decomp$u[,2],
-  decomp$u[,3],
-  type = "n",
-  xlab = "Second component",
-  ylab = "Third component",
-  xlim = c(min(decomp$u[,2]) - 0.2,max(decomp$u[,2]) + 0.2)
-)
-text(
-  x      = decomp$u[,2],
-  y      = decomp$u[,3],
-  labels = species
-)
-
-graphics.off()
-
-
 postscript("results/acid_difference.eps",width=5,height=5,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
 
 plot(
@@ -44,11 +25,16 @@ plot(
   xlab = "Second component",
   ylab = "Third component"
 )
-text(
-  x      = decomp$v[,2],
-  y      = decomp$v[,3],
-  labels =  levels(data$acid)
-)
+symbols(0,0,circles=0.3,add=TRUE,lty=2,fg="grey")
+abline(h=0,lty=2,col="grey")
+abline(v=0,lty=2,col="grey")
 
+weights <- data$weight[order(data$acid[1:20])]
+points(
+  decomp$v[,2],
+  decomp$v[,3],
+  pch=22,
+  cex=(weights^(0.5) - 8)
+)
 
 graphics.off()

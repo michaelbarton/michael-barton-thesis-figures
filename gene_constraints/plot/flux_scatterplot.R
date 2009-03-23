c70dadd70704ca4134847cc57b6a3f90d5926748
Michael Barton <mail@michaelbarton.me.uk>
2009-03-23 18:58:52 +0000

    Plot fluxes between environments

---
+++ gene_constraints/plot/flux_scatterplot.R
@@ -0,0 +1,22 @@
+rm(list=ls())
+library(lattice)
+source('helpers/find_replace.R')
+source('helpers/constraint_data.R')
+
+data <- constraint_data()
+data <- cast(data)
+
+plot <- splom(
+  ~ data[3:5],
+  groups  = data$setup,
+  xlab="Absolute reaction flux (log.2)",
+  ylab="Absolute reaction flux (log.2)",
+  auto.key = TRUE
+)
+
+
+postscript("results/flux_scatterplot.eps",width=7,height=7,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+
+print(plot)
+
+graphics.off()

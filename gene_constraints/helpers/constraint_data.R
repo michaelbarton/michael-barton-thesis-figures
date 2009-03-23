362bf07f0af5f5a5166a3cec40a08cdb30bd2a81
Michael Barton <mail@michaelbarton.me.uk>
2009-03-23 18:58:03 +0000

    Helper to process constraint data

---
+++ gene_constraints/helpers/constraint_data.R
@@ -0,0 +1,27 @@
+
+constraint_data <- function(){
+  library(reshape)
+
+  data <- read.csv(file='data/gene_constraint.csv',header=TRUE)
+
+  scaled <- cast( melt(data,measure.var="flux"), gene + setup ~ environment + variable)
+
+  # ignore rows where all values are very close to zero or the same across environments
+  scaled <- subset(scaled, !(abs(glc_flux) < 0.001 & abs(amm_flux) < 0.001 & abs(sul_flux) < 0.001))
+  scaled <- subset(scaled, !(glc_flux == amm_flux & amm_flux == sul_flux))
+
+  # convert back to long data frame
+  class(scaled) <- "data.frame"
+  scaled <- melt(scaled,id.var=c("gene","setup"))
+
+  # Take absolute log of each value
+  scaled$value <- log2(abs(scaled$value + 0.00001))
+
+  # Replace environment name
+  scaled$variable <- find.replace(scaled$variable,
+    c("glc_flux","amm_flux","sul_flux"),
+    c("glucose", "ammonium","sulphate")
+  )
+
+  scaled
+}

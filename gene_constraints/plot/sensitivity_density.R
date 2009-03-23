acf35ba6df76de6ad323bedd14b3fcbb1d48c2ea
Michael Barton <mail@michaelbarton.me.uk>
2009-03-23 20:37:28 +0000

    Update plotting of sensitivity density

--- gene_constraints/plot/sensitivity_density.R
+++ gene_constraints/plot/sensitivity_density.R
@@ -1,37 +1,26 @@
 rm(list=ls())
+library(lattice)
+source('helpers/find_replace.R')
 
 data <- read.csv(file='data/gene_sensitivity.csv',header=TRUE)
 
 data$sensitivity <- log10(abs(data$sensitivity))
 
-postscript("results/sensitivity_density.eps",width=5,height=5,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+# Replace environment name
+data$environment <- find.replace(data$environment,
+  c("glc",     "amm",     "sul"),
+  c("glucose", "ammonium","sulphate")
+)
 
-plot(
-  density(data$sensitivity),
-  type="n",
+
+plot <- densityplot(
+  ~ sensitivity | setup,
+  groups = environment,
+  auto.key = TRUE,
   xlab="Absolute reaction sensitivity (log10)",
-  main="",
-  ylim=c(0,0.15)
+  data=data
 )
 
-environments <- unique(data$environment)
-line_types <- c(2,1,3)
-colors <- c("grey20","grey40",'black')
-
-legend(
-  'topright',
-  legend = environments,
-  lty    = line_types,
-  col    = colors
-)
-
-for(i in 1:length(environments)){
-  subset = data$sensitivity[data$environment == environments[i]]
-  lines(
-    density(subset),
-    lty = line_types[i],
-    col = colors[i]
-  )
-}
-
+postscript("results/sensitivity_density.eps",width=9,height=5,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+print(plot)
 graphics.off()

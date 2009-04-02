bbbe61b72c4c9df64773e82c64d99c96040fa620
Michael Barton <mail@michaelbarton.me.uk>
2009-04-02 18:29:52 +0100

    Updated symbol colors in flux scatterplot

--- gene_constraints/plot/flux_scatterplot.R
+++ gene_constraints/plot/flux_scatterplot.R
@@ -7,11 +7,17 @@ data <- flux_data()
 data <- cast(data)
 
 plot <- splom(
-  ~ data[3:5],
+  ~ data[4:6],
   groups  = data$setup,
-  xlab="Absolute reaction flux (log.2)",
-  ylab="Absolute reaction flux (log.2)",
-  auto.key = TRUE
+  xlab= "Absolute reaction flux (log.2)",
+  ylab= "Absolute reaction flux (log.2)",
+  col = c("grey10","grey50"),
+  pch = c(1,2),
+  key = list(
+    columns = 2, 
+    points  = list(pch = c(1,2), col = c("grey10","grey50")),
+    text    = list(c("Optimal", "Suboptimal"))
+  )
 )
 
 

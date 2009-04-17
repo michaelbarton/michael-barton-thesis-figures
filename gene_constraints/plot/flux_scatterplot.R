4860fbeff44cb8b8661eff7c430f4c47395c2b7e
Michael Barton <mail@michaelbarton.me.uk>
2009-04-17 12:11:35 +0100

    Plot sub/optimal fluxes on alternate sides of splom

--- gene_constraints/plot/flux_scatterplot.R
+++ gene_constraints/plot/flux_scatterplot.R
@@ -8,14 +8,19 @@ data <- cast(data)
 
 plot <- splom(
   ~ data[4:6],
-  groups  = data$setup,
   xlab= "Absolute reaction flux (log.2)",
   ylab= "Absolute reaction flux (log.2)",
-  col = c("grey10","grey50"),
-  pch = c(1,2),
+  upper.panel=function(x,y,...){
+    values = data$setup == "optimal"
+    panel.xyplot(x[values],y[values],col="grey10",pch=1)
+  },
+  lower.panel=function(x,y,...){
+    values = data$setup == "suboptimal"
+    panel.xyplot(x[values],y[values],col="grey20",pch=2)
+  },
   key = list(
-    columns = 2, 
-    points  = list(pch = c(1,2), col = c("grey10","grey50")),
+    columns = 2,
+    points  = list(pch = c(1,2), col = c("grey10","grey20")),
     text    = list(c("Optimal", "Suboptimal"))
   )
 )

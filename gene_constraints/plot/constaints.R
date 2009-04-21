11c41f67d00c20f7bb7fd2835a3379bfb8dcc130
Michael Barton <mail@michaelbarton.me.uk>
2009-04-22 00:49:13 +0100

    Updated constraints plot for both environments

--- gene_constraints/plot/constaints.R
+++ gene_constraints/plot/constaints.R
@@ -3,34 +3,27 @@ library(reshape)
 library(limma)
 library(lattice)
 
-find.replace <- function(vector,find,replace.with){
-  if(length(find) == length(replace.with)){
-    for(i in 1:length(find)){
-      index <- which(levels(vector) == find[i])
-      levels(vector)[index] <- replace.with[i]
-    }
-  } else {
-    stop("Find and replace vectors should be same length")
-  }
-  vector
-}
-
+source('helpers/find_replace.R')
 data <- read.csv(file='data/gene_constraint.csv',header=TRUE)
 
 melted      <- melt(data,measure.var=c('reaction'))
-convergence <- cast(melted, gene + type ~ environment + variable)
-convergence[,3:5] <- ! is.na(convergence[,3:5])
+convergence <- cast(melted, gene + setup + type ~ environment + variable)
+convergence[,4:6] <- ! is.na(convergence[,4:6])
 
 result <- data.frame(
   environment = character(0),
   type        = character(0),
+  setup       = character(0),
   counts      = numeric(0)
 )
 
 # Venn type counts of environmental reaction use
 types <- unique(data$type)
+setups <- unique(data$setup)
+
+for(k in 1:length(setups)){
 for(i in 1:length(types)){
-  counts <- as.data.frame(vennCounts(subset(convergence,type == types[i])[,3:5])[,1:4])
+  counts <- as.data.frame(vennCounts(subset(convergence,setup == setups[k] & type == types[i])[,4:6])[,1:4])
   names(counts)[1:3] <- levels(data$environment)
 
   # Replace 1s with name of environment
@@ -48,16 +41,17 @@ for(i in 1:length(types)){
   result <- rbind(result,
     data.frame(
       environment = counts$environment,
+      setup       = setups[k],
       type        = types[i],
-      count      = counts$Count
+      count       = counts$Count
     )
   )
 }
-
+}
 
 # Replace names of environments
 result$environment <- find.replace(result$environment,
-  c('glcnitsul', 'glc',           'nit',            'sul',            'nitsul', 'glcsul', 'glcnit'),
+  c('ammglcsul', 'glc',           'amm',            'sul',            'ammsul', 'glcsul', 'ammglc'),
   c('all',       'glucose\nonly', 'ammonium\nonly', 'sulphate\nonly', 'mixed',  'mixed',  'mixed' )
 )
 
@@ -73,16 +67,22 @@ levels(ordered_types)[4] <- "sulphate\nonly"
 levels(ordered_types)[5] <- "mixed"
 result$environment <- factor(result$environment,levels=ordered_types)
 
+# Order solutions types in plot
+ordered_types <- as.ordered(1:2)
+levels(ordered_types)[1] <- "suboptimal"
+levels(ordered_types)[2] <- "optimal"
+result$setup <- factor(result$setup,levels=ordered_types)
+
 plot <- barchart(
-  count ~ environment | type,
+  count ~ environment | type + setup,
   data=subset(result,type != 'at maximum' & environment != "" ),
   ylab="Reaction frequency",
   xlab="Environment use",
   col="grey60"
 )
-plot$y.limits <- c(0,160)
+plot$y.limits <- c(0,175)
 
-postscript("results/constraints.eps",width=12,height=4,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+postscript("results/constraints.eps",width=12,height=8,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
 print(plot)
 graphics.off()
 

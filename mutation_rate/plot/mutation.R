d4f2fccef2e63a072decd115caa91689e1f62f2a
Michael Barton <mail@michaelbarton.me.uk>
2009-04-30 21:38:03 +0100

    Updated mutation rate plot with multiple cost types

--- mutation_rate/plot/mutation.R
+++ mutation_rate/plot/mutation.R
@@ -1,26 +1,33 @@
 rm(list=ls())
-require(MASS)
+require(lattice)
+source('helper/find_replace.R')
 
 data <- read.csv('data/acid_mutation_rates.csv')
+data <- subset(data, (cost_type == "weight" | cost_type == "glu-abs" | cost_type == "glu-rel"))
 
-postscript("results/mutation.eps",width=5,height=5,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
-
-plot(
-  data$weight,
-  data$mutation,
-  xlab="Molecular weight (Da)",
-  ylab="Mean mutation rate"
+data$cost_type <- find.replace(data$cost_type,
+  c("weight","glu-rel","glu-abs"),
+  c("Molecular weight","Glucose relative","Glucose absolute")
 )
 
-smooth <- loess(
-  mutation ~ weight,
-  data=data)
-lines(
-  x = sort(data$weight),
-  y = predict(smooth,sort(data$weight)),
-  lwd = 3,
-  lty = 2,
-  col = "grey"
+plot <- xyplot(
+  mutation ~ cost | cost_type,
+  data=data,
+  scale=list(relation="free"),
+  xlab="Amino acid cost",
+  ylab="Mean mutation rate",
+  ylim=c(0.85,1.2),
+  panel = function(x,y,...){
+    panel.loess(x,y,lty=2,lwd=3,col="grey50")
+    panel.xyplot(x,y)
+
+    cor <- cor.test(x,y,method="spear")
+    panel.text(min(x), 1.2,    paste("R = ",round(cor$estimate,3)),pos=4)
+    panel.text(min(x), 1.17,  paste("p = ",round(cor$p.value,3)),pos=4)
+  }
 )
 
+
+postscript("results/mutation.eps",width=4,height=10,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+print(plot)
 graphics.off()

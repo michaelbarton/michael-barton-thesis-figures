4b65d56c31ba8bb25458686ae772f4ac4c5bb7e8
Michael Barton <mail@michaelbarton.me.uk>
2009-04-30 20:42:18 +0100

    Updated fixation plot for multiple cost types

--- mutation_rate/plot/fixation.R
+++ mutation_rate/plot/fixation.R
@@ -1,26 +1,36 @@
+#!/usr/bin/env R
+
 rm(list=ls())
-require(MASS)
+require(lattice)
+source('helper/find_replace.R')
 
 data <- read.csv('data/acid_mutation_rates.csv')
+data <- subset(data, (cost_type == "weight" | cost_type == "glu-abs" | cost_type == "glu-rel"))
 
-postscript("results/fixation.eps",width=5,height=5,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
-
-plot(
-  data$weight,
-  data$fixed,
-  xlab="Molecular weight (Da)",
-  ylab="Percent sites fixed"
+data$cost_type <- find.replace(data$cost_type,
+  c("weight","glu-rel","glu-abs"),
+  c("Molecular weight","Glucose relative","Glucose absolute")
 )
 
-smooth <- loess(
-  fixed ~ weight,
-  data=data)
-lines(
-  x = sort(data$weight),
-  y = predict(smooth,sort(data$weight)),
-  lwd = 3,
-  lty = 2,
-  col = "grey"
+plot <- xyplot(
+  fixed ~ cost | cost_type,
+  data=data,
+  scale=list(relation="free"),
+  xlab="Amino acid cost",
+  ylab="Percent sites fixed",
+  ylim=c(0.65,1),
+  panel = function(x,y,...){
+    panel.loess(x,y,lty=2,lwd=3,col="grey50")
+    panel.xyplot(x,y)
+
+    cor <- cor.test(x,y,method="spear")
+    panel.text(min(x), 1,    paste("R = ",round(cor$estimate,3)),pos=4)
+    panel.text(min(x), 0.965, paste("p = ",round(cor$p.value,3)),pos=4)
+  }
 )
+
+postscript("results/fixation.eps",width=4,height=10,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+
+print(plot)
 
 graphics.off()

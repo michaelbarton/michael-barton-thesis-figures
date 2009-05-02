a8566730d087905b6d4c9602c34d090d2657e93c
Michael Barton <mail@michaelbarton.me.uk>
2009-05-02 19:18:15 +0100

    Updated amino acid cost rate density plot

--- mutation_rate/plot/rate_density.R
+++ mutation_rate/plot/rate_density.R
@@ -21,8 +21,24 @@ plot <- densityplot(
     scale="free",
     xlab="Phylogeny weighted amino acid cost",
     ylab="Density",
+    ylim=list(c(0,0.59),c(0,260),c(0,0.077)),
     groups=fixed,
-    auto.key=TRUE
+    auto.key=TRUE,
+    key=list(
+      columns = 2,
+      text    = list(c("Fixed", "Varying")),
+      lines   = list(lty=c(2,1))
+    ),
+    panel=function(x,subscripts){
+      fixed   <- subset(data[subscripts,],fixed == "fixed")
+      varying <- subset(data[subscripts,],fixed == "varying")
+
+      fixed   <- density(fixed$cost)
+      varying <- density(varying$cost)
+
+      panel.lines(fixed$x,fixed$y,lty=2,lwd=1,col="grey30")
+      panel.lines(varying$x,varying$y,col="grey30")
+    }
 )
 
 postscript("results/cost_density.eps",width=4,height=10,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")

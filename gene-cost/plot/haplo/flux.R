9b8be8b8a28fb9c2b281378da05635054b388b93
Michael Barton <mail@michaelbarton.me.uk>
2009-06-10 14:51:44 +0100

    Haplo-flux plot. Change units, and spearmans rank values

--- gene-cost/plot/haplo/flux.R
+++ gene-cost/plot/haplo/flux.R
@@ -2,6 +2,7 @@ rm(list=ls())
 library(lattice)
 
 source('helper/find_replace.R')
+source('helper/panel_functions.R')
 
 data <- read.csv(file='data/haplo_flux.csv')
 data$flux <- log2(abs(data$flux) + 0000.1)
@@ -19,9 +20,14 @@ plot <- xyplot(
   haploinsufficiency ~ flux | environment + solution,
   data = data,
   xlab = "log. Reaction flux",
-  ylab = "Haplosufficient growth rate"
+  ylab = "Hemizygous growth rate difference",
+  panel= function(x,y,...){
+    panel.xyplot(x,y,...)
+    panel.abline(h=0,col="grey50",lty=2)
+    panel.spearman(x,y)
+  }
 )
 
-postscript("results/haplo_flux.eps",width=6,height=6,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+postscript("results/haplo/flux.eps",width=8,height=8,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
 print(plot)
 graphics.off()

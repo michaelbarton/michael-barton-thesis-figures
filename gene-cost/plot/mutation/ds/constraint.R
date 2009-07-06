1e24a48e55cdab80da8948275641695a08594a52
Michael Barton <mail@michaelbarton.me.uk>
2009-07-06 11:50:14 +0100

    Created plots for ds mutation rate

---
+++ gene-cost/plot/mutation/ds/constraint.R
@@ -0,0 +1,32 @@
+rm(list=ls())
+library(lattice)
+
+source('helper/find_replace.R')
+source('helper/panel_functions.R')
+
+
+data <- read.csv(file='data/mutation/constraint.csv')
+
+order <- as.ordered(1:2)
+levels(order) <- c('suboptimal','optimal')
+data$solution <- factor(data$solution,levels=order)
+
+data$environment <- find.replace(data$environment,
+  c('glc'     , 'amm'      , 'sul'), 
+  c('glucose' , 'ammonium' , 'sulfur')
+)
+
+plot <- bwplot(
+  dS ~ constraint | environment + solution,
+  data = data,
+  ylab = "dS",
+  xlab = "Reaction constraint",
+  panel= function(x,y,...){
+    panel.bwplot(x,y,...)
+    panel.anova(x,y)
+  }
+)
+
+postscript("results/mutation/ds/constraint.eps",width=10,height=6,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+print(plot)
+graphics.off()
---
+++ gene-cost/plot/mutation/ds/sensitivity.R
@@ -0,0 +1,32 @@
+rm(list=ls())
+library(lattice)
+
+source('helper/find_replace.R')
+source('helper/panel_functions.R')
+
+data <- read.csv(file='data/mutation/sensitivity.csv')
+data$sensitivity <- log(abs(data$sensitivity) + 0.0001)
+
+order <- as.ordered(1:2)
+levels(order) <- c('suboptimal','optimal')
+data$solution <- factor(data$solution,levels=order)
+
+data$environment <- find.replace(data$environment,
+  c('glc'     , 'amm'      , 'sul'), 
+  c('glucose' , 'ammonium' , 'sulfur')
+)
+
+plot <- xyplot(
+  dS ~ sensitivity | environment + solution,
+  data = data,
+  xlab = "log. Reaction sensitivity",
+  ylab = "dS",
+  panel= function(x,y,...){
+    panel.xyplot(x,y,...)
+    panel.spearman(x,y)
+  }
+)
+
+postscript("results/mutation/ds/sensitivity.eps",width=10,height=6,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+print(plot)
+graphics.off()

9b44f6d20c742c670b9ddc8bbbc7022adb4c1bd8
Michael Barton <mail@michaelbarton.me.uk>
2009-07-09 14:56:15 +0100

    Updated R^2 plotting. Removed adjusted r square values

--- gene-cost/helper/panel_functions.R
+++ gene-cost/helper/panel_functions.R
@@ -2,7 +2,7 @@ library(grid)
 
 panel.anova <- function(x,y){
   aov_summary <- summary.lm(aov(y ~ x))
-  r_sq <- round(aov_summary$adj.r.squared, digits = 3)
+  r_sq <- round(aov_summary$r.squared, digits = 3)
   p    <- round(1 - pf(
     aov_summary$fstatistic[1],
     aov_summary$fstatistic[2],
@@ -10,7 +10,7 @@ panel.anova <- function(x,y){
     digits = 3
   )
 
-  panel.regression.values(r_sq,p)
+  panel.regression.values(r_sq,p,TRUE)
 }
 
 panel.binomial <- function(x,y,lwd=2,col="grey50"){
@@ -26,7 +26,7 @@ panel.binomial <- function(x,y,lwd=2,col="grey50"){
   cor <- cor.test(predict(model,list(x=x),type="response"),y,method="spear")
   r_sq <- round(cor$estimate^2,3)
 
-  panel.regression.values(r_sq,p)
+  panel.regression.values(r_sq,p,TRUE)
 
 }
 
@@ -37,11 +37,14 @@ panel.spearman <- function(x,y){
   panel.regression.values(r_sq,p)
 }
 
-panel.regression.values <- function(r.square,p.value){
-  grid.text(
-    paste('R = ',r.square,', p = ',p.value),
-    x=unit(0.075,"npc"),
-    y=unit(0.925,"npc"),
-    just='left'
-  )
+panel.regression.values <- function(r.value,p.value,squared=FALSE){
+  if(squared == TRUE){
+    r = bquote(R^2 ==  .(r.value))
+  } else {
+    r = bquote(R ==  .(r.value))
+  }
+  p = bquote(p ==  .(p.value))
+
+  grid.text(r, x=unit(0.075,"npc"), y=unit(0.925,"npc"), just='left')
+  grid.text(p, x=unit(0.4,"npc"), y=unit(0.905,"npc"), just='left')
 }

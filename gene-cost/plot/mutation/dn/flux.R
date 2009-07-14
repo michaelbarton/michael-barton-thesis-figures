dc9afd847ec1c9a417276b4a6f1cedf7b8f61cf9
Michael Barton <mail@michaelbarton.me.uk>
2009-07-14 15:11:03 +0100

    Plots for comparison of dn with gene importance

---
+++ gene-cost/plot/mutation/dn/flux.R
@@ -0,0 +1,38 @@
+rm(list=ls())
+library(lattice)
+
+source('helper/find_replace.R')
+source('helper/panel_functions.R')
+
+data <- read.csv(file='data/mutation/flux.csv')
+data$flux <- log2(abs(data$flux) + 0000.1)
+
+order <- as.ordered(1:2)
+levels(order) <- c('suboptimal','optimal')
+data$solution <- factor(data$solution,levels=order)
+
+data$environment <- find.replace(data$environment,
+  c('glc'     , 'amm'      , 'sul'), 
+  c('glucose' , 'ammonium' , 'sulfur')
+)
+
+ordered_environments <- as.ordered(1:3)
+levels(ordered_environments)[1] <- "glucose"
+levels(ordered_environments)[2] <- "ammonium"
+levels(ordered_environments)[3] <- "sulfur"
+data$environment <- factor(data$environment,levels=ordered_environments)
+
+plot <- xyplot(
+  dN ~ flux | environment + solution,
+  data = data,
+  xlab = "log. Reaction flux",
+  ylab = "dN",
+  panel= function(x,y,...){
+    panel.xyplot(x,y,...)
+    panel.spearman(x,y)
+  }
+)
+
+postscript("results/mutation/dn/flux.eps",width=10,height=6,onefile=FALSE,horizontal=FALSE, paper = "special",colormodel="rgb")
+print(plot)
+graphics.off()

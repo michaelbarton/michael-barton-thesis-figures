8b4a99135180c0fb6720c7e1c72552cec492884d
Michael Barton <mail@michaelbarton.me.uk>
2009-07-21 14:52:45 +0100

    Updated formatting of p-values for amino acid usage

---
+++ helper/panel_functions.R
@@ -0,0 +1,64 @@
+library(grid)
+
+panel.anova <- function(x,y){
+  aov_summary <- summary.lm(aov(y ~ x))
+  r_sq <- aov_summary$r.squared
+  p    <- 1 - pf(
+    aov_summary$fstatistic[1],
+    aov_summary$fstatistic[2],
+    aov_summary$fstatistic[3]
+  )
+
+  panel.regression.values(r_sq,p,TRUE)
+}
+
+panel.binomial <- function(x,y,lwd=2,col="grey50"){
+  model <- glm(y ~ x,binomial)
+
+  continuous <- seq(min(x),max(x),(max(x) - min(x)) * 0.01)
+  predicted <- predict(model,list(x=continuous),type="response")
+  panel.lines(continuous,predicted,lwd=lwd,col=col)
+
+  model.sum <- summary.glm(model)
+  p <- model.sum$coefficients[2,4]
+
+  cor <- cor.test(predict(model,list(x=x),type="response"),y,method="spear")
+  r_sq <- cor$estimate^2
+
+  panel.regression.values(r_sq,p,TRUE)
+
+}
+
+panel.confidence <- function(x,y,lwd=2,col="grey50"){
+
+  model = rlm(y ~ x)
+  continuous <- seq(min(x),max(x),(max(x) - min(x)) * 0.01)
+  predicted <- predict(model,data.frame(x=continuous))
+  confidence <- predict(model,data.frame(x=continuous),level = 0.95, interval = "confidence")
+
+  panel.lines(continuous,predicted,lwd=lwd,col=col)
+  panel.lines(continuous,confidence[,2],lty=2,lwd=lwd,col=col)
+  panel.lines(continuous,confidence[,3],lty=2,lwd=lwd,col=col)
+}
+
+panel.spearman <- function(x,y){
+  cor <- cor.test(x,y,method="spear")
+  r_sq <- cor$estimate
+  p    <- cor$p.value
+  panel.regression.values(r_sq,p)
+}
+
+panel.regression.values <- function(r.value,p.value,squared=FALSE){
+  formatted_r = round(r.value,digits=3)
+  formatted_p = format(p.value,scientific=TRUE,digits=2)
+
+  if(squared == TRUE){
+    r = bquote(R^2 ==  .(formatted_r))
+  } else {
+    r = bquote(R ==  .(formatted_r))
+  }
+  p = bquote(p ==  .(formatted_p))
+
+  grid.text(r, x=unit(0.075,"npc"), y=unit(0.925,"npc"), just='left')
+  grid.text(p, x=unit(0.45,"npc"), y=unit(0.923,"npc"), just='left')
+}
